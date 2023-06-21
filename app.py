31613f076ffdbf893c962e3713d4a164d52b2454
Kaue Araujo <Kauesantos.araujo@gmail.com>
2023-06-21 19:14:40 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+frutas = []
+registros = []
+
+@app.route('/', methods=["GET", "POST"])
+def principal():
+	#frutas = ['Banana', 'Maça', 'Laranja', 'Uva', 'Melancia']
+	if request.method == "POST":
+		if request.form.get("fruta"):
+			frutas.append(request.form.get("fruta"))
+	return render_template("index.html", frutas=frutas)
+
+
+@app.route('/sobre', methods=["GET", "POST"])
+def sobre():
+	#notas = {"Kaue":10, "Ana":9, "Maju":8, "Manu":7}
+	if request.method == "POST":
+		if request.form.get("aluno") and request.form.get("nota"):
+			registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
+	return render_template("sobre.html", registros=registros)
+
+app.run(debug=True)
